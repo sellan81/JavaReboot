d0649c607f4c5e9d161101df7aff5824e694f5e1
”Alan <sell@ngs.ru>
2021-01-14 12:36:09 +0700

    Author: Alan Pakhomov удалены лишние комментарии

--- src/Task1/PetroleumProductsCalc.java
+++ src/Task1/PetroleumProductsCalc.java
@@ -3,6 +3,7 @@ package Task1;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//Класс учета ГСМ на предпрятии
 public class PetroleumProductsCalc {
     public static  String[] inputDataArray = {"C100_1-100", "C200_1-120-1200", "C300_1-120-30", "C400_1-80-20", "C100_2-50", "C200_2-40-1000", "C300_2-200-45", "C400_2-10-20", "C100_3-10", "C200_3-170-1100", "C300_3-150-29", "C400_3-100-28", "C100_1-300", "C200_1-100-750", "C300_1-32-15"};
     public static final String delim = "_";
@@ -142,6 +143,8 @@ public class PetroleumProductsCalc {
         sortArr();
     }
 
+
+    //Класс данных об автомобилях
     private static class DataCars {
         private String typeOfCar;
         private int namberCar;
